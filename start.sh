3ad639ad38632420c0f385a473c7b2f78e8c3492
Harshulsen <150106608+Harshulsen@users.noreply.github.com>
2024-01-09 11:50:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/VJ-FILTER-BOT.git /VJ-FILTER-BOT 
+  git clone https://github.com/Harshulsen/Matrix-auto-filter /Matrix-auto-filter 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /VJ-FILTER-BOT 
+  git clone $UPSTREAM_REPO /Matrix-auto-filter 
 fi
-cd /VJ-FILTER-BOT 
+cd /Matrix-auto-filter 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
